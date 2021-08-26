4ef094c37c88379e8c0d0229715a591dde11342b
nicolafelice <felicenicola99@hotmail.com>
2021-08-26 17:59:58 +0200

    surname validation

--- app.js
+++ app.js
@@ -2,8 +2,41 @@
 // creo array lista cognomi
 const listaCognomi = ['bianchi', 'neri', 'rossi', 'verdi', 'gialli'];
 
-// chiedi all’utente il cognome e inseriscilo in un array con gli altri cognomi
-const cognomeUtente = prompt('inserisci il tuo cognome').toLowerCase();
+// chiedi all’utente il cognome e salvalo
+let cognomeUtente = prompt('inserisci il tuo cognome').toLowerCase();
+
+// funzione che controlla la validità dell'input
+function validateUserSurname() {
+
+    if (cognomeUtente == "") {
+        return true;
+    }
+
+    // check if it contains numbers
+    for (let i = 0; i < cognomeUtente.length; i++) {
+        if ( !isNaN(cognomeUtente[i]) ) {
+            return true;
+        }
+    }
+
+    // checking if it includes invalid characters
+    const invalidCharacters = [" ", "!", "”", "#", "$", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "^", "]", "_", "`", "|", "{", "~", "}", "\\"];
+    for (let i = 0; i < invalidCharacters.length; i++) {
+        if (cognomeUtente.includes(invalidCharacters[i])) {
+            return true;
+        }        
+    }
+
+    // reurn false if the input is valid
+    return false;
+}
+
+// fai reinserire il cognome finchè non è valido
+while (validateUserSurname()) {
+    cognomeUtente = prompt("errore, inserisci il tuo cognome in modo valido!").toLowerCase();
+}
+
+// una volta validato inseriscilo con gli altri cognomi
 listaCognomi.push(cognomeUtente);
 
 // riordina la lista cognomi alfabeticamente
